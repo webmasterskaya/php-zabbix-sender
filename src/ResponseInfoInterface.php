6550518272a301f8f115bf2b0d9280ceea5177a7
Artem Vasilev <kern.usr@gmail.com>
2025-04-12 01:37:33 +0300

    Add methods to ResponseInfoInterface

--- src/ResponseInfoInterface.php
+++ src/ResponseInfoInterface.php
@@ -4,5 +4,11 @@ namespace Webmasterskaya\ZabbixSender;
 
 interface ResponseInfoInterface
 {
+	public function getProcessed(): ?int;
 
+	public function getFailed(): ?int;
+
+	public function getSpent(): ?float;
+
+	public function getTotal(): ?int;
 }
